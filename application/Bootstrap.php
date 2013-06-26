c3273e8573eec860a226524202105993aedeaf92
emersonrodrigod <emersonrodrigod@gmail.com>
2013-06-26 11:11:32 -0300

    Commit inicial do projeto agilleStore

---
+++ application/Bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
+
+    protected function _initAutoLoader() {
+        $autoLoader = Zend_Loader_Autoloader::getInstance();
+        $autoLoader->setFallbackAutoloader(true); //Loader de todas as classes
+    }
+    
+     protected function _initPlugins() {
+        $bootstrap = $this->getApplication();
+
+        if ($bootstrap instanceof Zend_Application) {
+            $bootstrap = $this;
+        }
+
+        $bootstrap->bootstrap('FrontController');
+        $front = $bootstrap->getResource('FrontController');
+        $front->registerPlugin(new Webagille_Plugins_Layout());
+        //$front->registerPlugin(new My_Plugins_CheckAcl());
+    }
+
+}
+
